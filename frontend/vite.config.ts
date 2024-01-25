29f0094d01a06393df6b04d33da6a8327ee47f9c
xiaoying xie <xiaoyingxie@deepglint.com>
2024-01-25 18:12:27 +0800

    前端框架搭建

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+/*
+ * @Description: Stay hungry，Stay foolish
+ * @Author: leslie
+ * @Date: 2024-01-25 16:05:02
+ * @LastEditors: leslie
+ * @LastEditTime: 2024-01-25 16:06:25
+ * 佛祖保佑没bug
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000
+  }
+})
