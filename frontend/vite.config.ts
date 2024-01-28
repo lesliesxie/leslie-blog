51d703ef60af5a2e4471f2131e339a517459ac71
xie xiaoying <xiaoyingxie@deepglint.com>
2024-01-28 22:53:57 +0800

    npm some plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,18 @@
  * @Author: leslie
  * @Date: 2024-01-25 16:05:02
  * @LastEditors: leslie
- * @LastEditTime: 2024-01-26 18:59:52
+ * @LastEditTime: 2024-01-28 22:39:00
  * 佛祖保佑没bug
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3001
-  }
+  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') }},
 })
