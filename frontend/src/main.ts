fc69f48a23c0514b933a87627600128e980da3da
xie xiaoying <xiaoyingxie@deepglint.com>
2024-01-29 22:28:57 +0800

    fix ts unable to parse .vue files and report errors bug

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+/*
+ * @Description: Stay hungry，Stay foolish
+ * @Author: leslie
+ * @Date: 2024-01-28 19:16:00
+ * @LastEditors: leslie
+ * @LastEditTime: 2024-01-29 22:23:03
+ * 佛祖保佑没bug
+ */
+import { createApp } from "vue";
+import "./style/public.less";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
