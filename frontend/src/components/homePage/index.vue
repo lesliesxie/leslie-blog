aec81075e6d9b7c932fac65e885f21a462771870
xie xiaoying <xiaoyingxie@deepglint.com>
2024-01-31 23:22:03 +0800

    import element plus

--- frontend/src/components/homePage/index.vue
+++ frontend/src/components/homePage/index.vue
@@ -3,40 +3,57 @@
  * @Author: leslie
  * @Date: 2024-01-28 22:13:18
  * @LastEditors: leslie
- * @LastEditTime: 2024-01-30 23:33:07
+ * @LastEditTime: 2024-01-31 23:12:24
  * 佛祖保佑没bug
 -->
 <template>
   <div class="home-page">
     <div class="top-nav">
-      <!-- //TODO  svg -->
-      <svg-icon name="user"></svg-icon>
+      <el-menu
+        class="leslie-menu"
+        :default-active="activeIndex"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2">首页123</el-menu-item>
+      </el-menu>
     </div>
     <div class="center">
-      <div class="content">222</div>
+      <div class="content"></div>
       <div class="summary">333</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { ElMenu, ElMenuItem } from "element-plus";
+
+const activeIndex = ref("1");
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
 </script>
 
 <style lang="less" scoped>
 .home-page {
   width: 100%;
   height: calc(100vh);
+  position: fixed; //禁止上拉下拉露出白底
   .top-nav {
     width: 100%;
     height: 50px;
-    background-color: red;
   }
   .center {
+    margin: 10px 30px 0 30px;
+    height: calc(100vh - 60px);
+    width: calc(100vw - 60px);
+    display: flex;
     .content {
-      
+      width: calc(100% - 300px);
     }
     .summary {
-      
+      width: 300px;
     }
   }
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,13 @@
  * @Author: leslie
  * @Date: 2024-01-25 16:05:02
  * @LastEditors: leslie
- * @LastEditTime: 2024-01-29 23:19:44
+ * @LastEditTime: 2024-01-31 22:49:48
  * 佛祖保佑没bug
  */
 import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import vue from "@vitejs/plugin-vue";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import path from "path";
@@ -20,6 +23,13 @@ export default defineConfig({
       iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
       // Specify symbolId format
       symbolId: "icon-[dir]-[name]",
+    }),
+    // 按需引入element
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   server: {
