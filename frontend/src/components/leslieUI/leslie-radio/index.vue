41d55fdea3b79f56c467270e162dc4d4be84eb1a
xie xiaoying <xiaoyingxie@deepglint.com>
2024-03-13 21:59:52 +0800

    leslie-ui

---
+++ frontend/src/components/leslieUI/leslie-radio/index.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: Stay hungry，Stay foolish
+ * @Author: leslie
+ * @Date: 2024-03-13 21:20:08
+ * @LastEditors: leslie
+ * @LastEditTime: 2024-03-13 21:20:15
+ * 佛祖保佑没bug
+-->
+<template>
+  <div class="leslie-radio"></div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({});
+</script>
+
+<style lang="less" scoped>
+.leslie-radio {
+}
+</style>
