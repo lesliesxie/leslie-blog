2d17662f5b3b305f8172d86f7fb2b92ac521647e
xie xiaoying <xiaoyingxie@deepglint.com>
2024-03-30 22:11:02 +0800

    add event-bus

---
+++ frontend/src/global/event-bus.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: Stay hungry，Stay foolish
+ * @Author: leslie
+ * @Date: 2024-03-30 20:05:03
+ * @LastEditors: leslie
+ * @LastEditTime: 2024-03-30 20:14:37
+ * 佛祖保佑没bug
+ */
+
+import mitt from "mitt";
+const bus = mitt();
+export default bus;
