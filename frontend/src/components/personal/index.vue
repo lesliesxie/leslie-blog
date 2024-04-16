b06c6d67bebcd70193b371ef551d0f1ca3272858
xie xiaoying <xiaoyingxie@deepglint.com>
2024-04-16 22:55:06 +0800

    feat: update user

--- frontend/src/components/personal/index.vue
+++ frontend/src/components/personal/index.vue
@@ -3,7 +3,7 @@
  * @Author: leslie
  * @Date: 2024-04-14 19:46:34
  * @LastEditors: leslie
- * @LastEditTime: 2024-04-15 22:39:43
+ * @LastEditTime: 2024-04-16 22:54:03
  * 佛祖保佑没bug
 -->
 
@@ -27,18 +27,19 @@
           <div class="right">
             <div class="userName">
               <span>用户名</span>
-              <!-- TODO 实现v-model -->
-              <leslie-input v-model="userName"></leslie-input>
+              <leslie-input v-model="user.userName"></leslie-input>
             </div>
             <div class="password">
               <span>密码</span>
-              <leslie-input v-model="password"></leslie-input>
+              <leslie-input v-model="user.password"></leslie-input>
             </div>
             <div class="sign">
               <span>个性签名</span>
               <leslie-input></leslie-input>
             </div>
-            <leslie-button btnType="primary">修改</leslie-button>
+            <leslie-button class="edit" @click="onEdit" btnType="primary"
+              >修改</leslie-button
+            >
           </div>
         </div>
       </template>
@@ -47,22 +48,35 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import bus from "@/global/event-bus";
-import { getUser } from "@/server";
+import { getUser, updateUser } from "@/server";
+import showMessage from "@/global/leslie-message";
 
 const personalVisible = ref(true);
-const userName = ref("");
-const password = ref("");
+let user = reactive({
+  id: 0,
+  userName: "",
+  password: "",
+});
 const dialogTitle = ref("个人中心");
 const avatar = ref("/src/assets/images/avatar.JPG");
 
+const onEdit = async () => {
+  await updateUser(user.id, user);
+  showMessage.success("修改成功");
+  localStorage.setItem("userName", user.userName);
+  dialogTitle.value = user.userName + "  的个人中心";
+  personalVisible.value = false;
+};
+
 onMounted(async () => {
   if (localStorage.getItem("isLogin") === "true") {
-    userName.value = localStorage.getItem("userName") as string;
-    dialogTitle.value = userName.value + "  的个人中心";
-    let data = await getUser(userName.value);
-    password.value = data.password;
+    user.userName = localStorage.getItem("userName") as string;
+    dialogTitle.value = user.userName + "  的个人中心";
+    let data = await getUser(user.userName);
+    // 对象赋值不更改会页面无法及时渲染
+    user = Object.assign(user, data);
   }
   bus.on("changeVisible", (value) => {
     personalVisible.value = value as boolean;
@@ -95,6 +109,9 @@ onMounted(async () => {
         span {
           width: 70px;
         }
+      }
+      .edit {
+        margin-top: 24px;
       }
     }
   }
